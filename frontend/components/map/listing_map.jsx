de5844cdf42ce73df8b8ee3e30155c830335801b
Mark Kopec <mark.kopec@gmail.com>
2018-12-18 09:32:53 -0800

    Add listing map component

---
+++ frontend/components/map/listing_map.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class ListingMap extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+
+}
+
+render() {
+
+  return (
+    <div className="map" ref="map">
+
+    </div>
+  )
+}
+
+
+export default ListingMap;
