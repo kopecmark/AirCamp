1177fece61e1c0320733514b536f6bb148cbb0df
Mark Kopec <mark.kopec@gmail.com>
2018-12-17 16:55:11 -0800

    Correct routes to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resource :user, only: [:create]
+    resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
     resources :listings, only: [:index, :show]
   end
