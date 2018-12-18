ac582894bb489ea206f7c61576f1c6f835ec9b39
Mark Kopec <mark.kopec@gmail.com>
2018-12-18 13:38:22 -0800

    Add listing map component

--- frontend/components/map/listing_map.jsx
+++ frontend/components/map/listing_map.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const mapOptions = {
+  center: {
+    lat: 37.773972,
+    lng: -122.431297
+  }, // San Francisco coords
+  zoom: 13
+};
 
 class ListingMap extends React.Component{
-  constructor(props) {
-    super(props)
+
+  componentDidMount() {
+    // wrap this.mapNode in a Google Map
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
   }
 
+  render() {
+    return (
+
+      <div className="map" ref={map => this.mapNode = map}> 
+      // this ref gives us access to the map dom node
+      MAP
+      </ div>
+    )
+  }
+    
 }
-
-render() {
-
-  return (
-    <div className="map" ref="map">
-
-    </div>
-  )
-}
-
 
 export default ListingMap;
