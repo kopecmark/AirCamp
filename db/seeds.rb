ce5c9e2acd2e40323604a130aaa4c549086bd79a
Mark Kopec <mark.kopec@gmail.com>
2018-12-17 21:11:37 -0800

    Add photo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,45 @@ listing1 = Listing.create({
   lng: 345345345.4,
   lat: 345345345.6
 })
+
+listing2 = Listing.create({
+  user_id: user1.id,
+  site_title: 'Pescadero State Beach',
+  description: "You'll be right on the pacific ocean, watch the stars and the ocean at the same time",
+  daily_cost: 60,
+  check_in_time: "3:00 pm",
+  check_out_time: "12:00 am",
+  capacity: 6,
+  lng: 345345345.4,
+  lat: 345345345.6
+})
+
+listing3 = Listing.create({
+  user_id: user1.id,
+  site_title: 'Rob Hill Campground',
+  description: "Stay at the top of the world it doesn't get much better then that",
+  daily_cost: 90,
+  check_in_time: "2:00 pm",
+  check_out_time: "10:00 am",
+  capacity: 4,
+  lng: 345345345.4,
+  lat: 345345345.6
+})
+
+listing4 = Listing.create({
+  user_id: user1.id,
+  site_title: 'Angel Island Campground',
+  description: "Find serenity and adventure just a quick jaunt from San Francisco with a night of camping at Angel Island State Park. Begin your Angel Island camping adventure by catching a ferry ride from Tiburon or Pier 41",
+  daily_cost: 45,
+  check_in_time: "3:00 pm",
+  check_out_time: "11:00 am",
+  capacity: 8,
+  lng: 345345345.4,
+  lat: 345345345.6
+})
+
+
+listing1.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite01-01.jpg'), filename: 'campsite01-01.jpg')
+listing2.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite02-01.jpg'), filename: 'campsite02-01.jpg')
+listing3.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite03-01.jpg'), filename: 'campsite03-01.jpg')
+listing4.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite04-01.jpg'), filename: 'campsite04-01.jpg')
