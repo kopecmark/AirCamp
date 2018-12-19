8ea3366bf31d52c548340dbeb41bdca2dd394fe9
Mark Kopec <mark.kopec@gmail.com>
2018-12-18 19:40:33 -0800

    Change defaul map controls

--- frontend/components/map/listing_map.jsx
+++ frontend/components/map/listing_map.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MarkerManager from '../../util/marker_manager';
 
 const mapOptions = {
   center: {
-    lat: 37.773972,
-    lng: -122.431297
+    lat: 37.780391,
+    lng: -122.4580377,
   }, // San Francisco coords
-  zoom: 13
+  zoom: 12,
+  mapTypeControl: false,
+  streetViewControl: false,
+  zoomControlOptions: {
+    position: google.maps.ControlPosition.TOP_RIGHT
+  },
+  fullscreenControl: false,
+  mapTypeId: 'terrain'
+  
 };
 
 class ListingMap extends React.Component{
-
+  constructor(props) {
+    super(props)
+  }
   componentDidMount() {
+    // set the map to show SF
+
     // wrap this.mapNode in a Google Map
     this.map = new google.maps.Map(this.mapNode, mapOptions);
+    // this.map = new google.maps.Map(mapDOMNode, mapOptions);
+    this.MarkerManager = new MarkerManager(this.map);
+    this.MarkerManager.updateMarkers(this.props.listings);
+
+  }
+
+  // regularListener() {
+  //   google.maps.event.addListener(this.map, 'idle', () => {
+
+  //     const { north, south, east, west } = this.map.getBounds().toJSON();
+  //     const bounds = {
+  //       northEast: { lat: north, lng: east },
+  //       southWest: { lat: south, lng: west }
+  //     }
+  //     this.props.updateFilters('bounds', bounds);
+  //   })
+  // }
+
+  componentDidUpdate() {
+    this.MarkerManager.updateMarkers(this.props.listings);
   }
 
   render() {
     return (
 
       <div className="map" ref={map => this.mapNode = map}> 
-      // this ref gives us access to the map dom node
-      MAP
+
       </ div>
     )
   }
