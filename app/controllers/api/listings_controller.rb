61f4c7f80158589a080eb30609653720dba6ca3f
Mark Kopec <mark.kopec@gmail.com>
2018-12-19 18:34:01 -0800

    Add show

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,6 +1,23 @@
 class Api::ListingsController < ApplicationController
   def index
     @listings = Listing.all
-    render :index
   end
+  
+  def show
+    @listing = Listing.with_attached_photos.find(params[:id])
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(
+      :site_title,
+      :description,
+      :daily_cost,
+      :check_in_time,
+      :check_out_time,
+      :capacity, 
+      
+    )
+  end
+  
 end
