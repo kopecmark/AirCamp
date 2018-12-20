e8d8ccf186b6ad990d83101f2018c75aaa18bd22
Mark Kopec <mark.kopec@gmail.com>
2018-12-19 18:31:41 -0800

    Add additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,30 @@ user1 = User.create({
   first_name: "Demo Person",
   last_name: "Demo Last Name",
   password: "demopassword",
-  user_img_url: "google/imgs/profile.jpg"
+  user_img_url: "https://images.homedepot-static.com/productImages/d31251a7-fcd6-4614-8608-a27d38496abd/svn/white-roommates-wall-decals-rmk1512gm-64_1000.jpg"
+})
+
+user2 = User.create({
+  email: "john@johnny.com",
+  first_name: "John",
+  last_name: "Samsun",
+  password: "johnpassword",
+  user_img_url: "https://i.pinimg.com/originals/bc/97/bb/bc97bb03ddecf00d516430ed1ca1dcbd.png"
+})
+
+user3 = User.create({
+  email: "deb@debby.com",
+  first_name: "Deb",
+  last_name: "Collons",
+  password: "debpassword",
+  user_img_url: "https://cdn.shopify.com/s/files/1/1518/3476/products/FiGPiN_86_Batgirl_BATMAN_CLASSIC_PIN.png?v=1530298842"
 })
 
 
+
+
 listing1 = Listing.create({
-  user_id: user1.id,
+  user_id: user2.id,
   site_title: 'Half Dome Summit',
   description: "Stay at the top of the world it doesn't get much better then that",
   daily_cost: 50,
@@ -34,8 +52,8 @@ listing2 = Listing.create({
   site_title: 'Pescadero State Beach',
   description: "You'll be right on the pacific ocean, watch the stars and the ocean at the same time",
   daily_cost: 60,
-  check_in_time: "3:00 pm",
-  check_out_time: "12:00 am",
+  check_in_time: "After 3:00 pm",
+  check_out_time: "Before 12:00 am",
   capacity: 6,
   lat: 37.263873,
   lng: -122.4141474
@@ -46,8 +64,8 @@ listing3 = Listing.create({
   site_title: 'Rob Hill Campground',
   description: "Stay at the top of the world it doesn't get much better then that",
   daily_cost: 90,
-  check_in_time: "2:00 pm",
-  check_out_time: "10:00 am",
+  check_in_time: "After 2:00 pm",
+  check_out_time: "Before 10:00 am",
   capacity: 4,
   lat: 37.7961521,
   lng: -122.4761422
@@ -58,8 +76,8 @@ listing4 = Listing.create({
   site_title: 'Angel Island Campground',
   description: "Find serenity and adventure just a quick jaunt from San Francisco with a night of camping at Angel Island State Park. Begin your Angel Island camping adventure by catching a ferry ride from Tiburon or Pier 41",
   daily_cost: 45,
-  check_in_time: "3:00 pm",
-  check_out_time: "11:00 am",
+  check_in_time: "After 3:00 pm",
+  check_out_time: "Before 11:00 am",
   capacity: 8,
   lat: 37.8570261,
   lng: -122.4414674
@@ -70,8 +88,8 @@ listing5 = Listing.create({
   site_title: 'Top of the Hill Serenity',
   description: "Find serenity and adventure just a quick jaunt from San Francisco with a night of camping at Angel Island State Park. Begin your Angel Island camping adventure by catching a ferry ride from Tiburon or Pier 41",
   daily_cost: 45,
-  check_in_time: "3:00 pm",
-  check_out_time: "11:00 am",
+  check_in_time: "After 3:00 pm",
+  check_out_time: "Before 11:00 am",
   capacity: 8,
   lat: 37.802974,
   lng: -122.406105
@@ -82,8 +100,8 @@ listing6 = Listing.create({
   site_title: 'Unlimited Golden Skies',
   description: "Find serenity and adventure just a quick jaunt from San Francisco with a night of camping at Angel Island State Park. Begin your Angel Island camping adventure by catching a ferry ride from Tiburon or Pier 41",
   daily_cost: 45,
-  check_in_time: "3:00 pm",
-  check_out_time: "11:00 am",
+  check_in_time: "After 3:00 pm",
+  check_out_time: "Before 11:00 am",
   capacity: 8,
   lat: 37.842960,
   lng: -122.499385
@@ -94,8 +112,8 @@ listing7 = Listing.create({
   site_title: 'Ocean Rock Wonderland',
   description: "Find serenity and adventure just a quick jaunt from San Francisco with a night of camping at Angel Island State Park. Begin your Angel Island camping adventure by catching a ferry ride from Tiburon or Pier 41",
   daily_cost: 45,
-  check_in_time: "3:00 pm",
-  check_out_time: "11:00 am",
+  check_in_time: "After 3:00 pm",
+  check_out_time: "Before 11:00 am",
   capacity: 8,
   lat: 37.788189,
   lng: -122.499955
@@ -106,8 +124,8 @@ listing8 = Listing.create({
   site_title: 'Desert Brush Oasis',
   description: "Find serenity and adventure just a quick jaunt from San Francisco with a night of camping at Angel Island State Park. Begin your Angel Island camping adventure by catching a ferry ride from Tiburon or Pier 41",
   daily_cost: 45,
-  check_in_time: "3:00 pm",
-  check_out_time: "11:00 am",
+  check_in_time: "After 3:00 pm",
+  check_out_time: "Before 11:00 am",
   capacity: 8,
   lat: 37.871304,
   lng: -122.199682
@@ -118,14 +136,20 @@ listing9 = Listing.create({
   site_title: 'Private Garden Oasis',
   description: "Find serenity and adventure just a quick jaunt from San Francisco with a night of camping at Angel Island State Park. Begin your Angel Island camping adventure by catching a ferry ride from Tiburon or Pier 41",
   daily_cost: 45,
-  check_in_time: "3:00 pm",
-  check_out_time: "11:00 am",
+  check_in_time: "After 3:00 pm",
+  check_out_time: "Before 11:00 am",
   capacity: 8,
   lat: 37.730316,
   lng: -122.463752
 })
 
 listing1.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite01-01.jpg'), filename: 'campsite01-01.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite01-02.jpg'), filename: 'campsite01-02.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite01-03.jpg'), filename: 'campsite01-03.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite01-04.jpg'), filename: 'campsite01-04.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite01-05.jpg'), filename: 'campsite01-05.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite01-06.jpg'), filename: 'campsite01-06.jpg')
+
 listing2.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite02-01.jpg'), filename: 'campsite02-01.jpg')
 listing3.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite03-01.jpg'), filename: 'campsite03-01.jpg')
 listing4.photos.attach(io: File.open('app/assets/images/campsite_photos/campsite04-01.jpg'), filename: 'campsite04-01.jpg')
