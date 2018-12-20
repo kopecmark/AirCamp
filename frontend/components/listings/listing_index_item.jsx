b7d9ebf9cd4c60d143289b87807f4717b4eca85c
Mark Kopec <mark.kopec@gmail.com>
2018-12-19 18:35:53 -0800

    No change

--- frontend/components/listings/listing_index_item.jsx
+++ frontend/components/listings/listing_index_item.jsx
@@ -4,9 +4,11 @@ import { Link, withRouter } from 'react-router-dom';
 const ListingIndexItem = ({ listing }) => (
   <li>
     <div className="listing-container">
-      <img className="listing-thumbnail" src={listing.photoUrls[0]} />
-      <h2>{listing.site_title}</h2>
-      ${listing.daily_cost}/night
+      <Link to={`listings/${listing.id}`}>
+        <img className="listing-thumbnail" src={listing.photoUrls[0]} />
+        <h2>{listing.site_title}</h2>
+        ${listing.daily_cost}/night
+      </ Link>
     </div>
   </li>
 )
