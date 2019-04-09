f416db4804ef18d9094775d174074cd9b9e6e4e7
Mark Kopec <mark.kopec@gmail.com>
2019-04-09 14:42:25 -0600

    Add booking route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
-    resources :listings, only: [:index, :show]
+    resources :listings, only: [:index, :show] do
+      resources :bookings, only: [:create]
+    end
   end
 
-  root "static_pages#root"
+  root to: "static_pages#root"
 end
