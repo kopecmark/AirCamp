70a09d108d7d6b367f54177b3e35a2e0dec341ca
Mark Kopec <mark.kopec@gmail.com>
2019-04-15 16:07:54 -0600

    Skip auth for development

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def create
     @user = User.find_by_credentials(
       params[:user][:email],
